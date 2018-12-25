a34763085bb5d32e06b25bf9f1107e74fc235061
lucky98622 <745478424@qq.com>
2018-12-25 23:56:42 +0800

    修改类图

--- src/cn/edu/neu/elevator/control/state/ElevatorState.java
+++ src/cn/edu/neu/elevator/control/state/ElevatorState.java
@@ -3,9 +3,6 @@ package cn.edu.neu.elevator.control.state;
 import cn.edu.neu.elevator.control.ElevatorController;
 
 public abstract class ElevatorState {
-
-
-
     private ElevatorController context;
 
     public void setContext(ElevatorController context) {
