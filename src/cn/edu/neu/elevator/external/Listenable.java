2a87aab7d911ab11d5f0a3d38af037431ca4948d
Cheelem <cheelem@outlook.com>
2018-12-25 21:14:41 +0800

    GUI methods encapsulation

--- src/cn/edu/neu/elevator/external/Listenable.java
+++ src/cn/edu/neu/elevator/external/Listenable.java
@@ -2,7 +2,6 @@ package cn.edu.neu.elevator.external;
 
 import cn.edu.neu.elevator.linstener.Listener;
 
-
 public interface Listenable {
     public void attachListener(Listener listener);
     public void detachListener(Listener listener);
--- src/cn/edu/neu/elevator/util/ElevatorLogger.java
+++ src/cn/edu/neu/elevator/util/ElevatorLogger.java
@@ -1,7 +1,17 @@
 package cn.edu.neu.elevator.util;
 
-public interface ElevatorLogger {
-    void info(String producer, String content);
-    void error(String producer, String content);
-    void warning(String producer, String content);
+import cn.edu.neu.elevator.display.GUIController;
+
+public class ElevatorLogger {
+    public static void info(String producer, String content) {
+        GUIController.submitLogText("INFO ["+producer+"] " + content);
+    }
+
+    public void error(String producer, String content) {
+        GUIController.submitLogText("ERROR ["+producer+"] " + content);
+    }
+
+    public void warning(String producer, String content) {
+        GUIController.submitLogText("WARNING ["+producer+"] " + content);
+    }
 }
